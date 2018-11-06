5c548efac97f8958a974d3dc53f020fa46110fd0
Alan Fuller <alan@fullworks.net>
2018-11-06 23:02:02 +0000

    Create transfer.sh

---
+++ transfer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash  
+printf "From Account /home/account, without /home/: "
+read -e account
+printf "From Database: "
+read -e db1
+printf "DB Pasword ( same on both): "
+read -e  pass
+printf "To db name: "
+read -e database2
+printf "To db user: "
+read -e user2
+printf "To server account:host: "
+read -e server
+printf "To target directory e.g. subdomain or public_html: "
+read -e dir
+
+mysqldump --add-drop-table --user=$account --password="${pass}" $db1 > /tmp/${account}_db.sql
+cat /tmp/${account}_db.sql|ssh $server "mysql --user=$user2 --password="${pass}" $database2  --"
+
+tar -czf /tmp/${account}.tar.gz -C /home/$account/public_html .
+cat /tmp/${account}.tar.gz | ssh $server "cd ${dir};tar -zxf -"
+
+
+rm -rf /tmp/$account.tar.gz
+rm -rf /tmp/${account}_db.sql
