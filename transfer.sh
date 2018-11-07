f131b1e05d90095da9d5109246a2eb8f286931b2
Alan Fuller <alan@fullworks.net>
2018-11-07 18:07:23 +0000

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash  
 read -er -p  "From Account /home/account, without /home/: " account
 read -er -p "From Database ( blank for none ): " db1
-if [ ! -z "$db1" ] then
+if [ ! -z "$db1" ] 
+then
   read -er -p "DB Pasword ( same on both): " pass
   read -er -p "To db name: " database2
   read -er -p "To db user: " user2
@@ -10,7 +11,8 @@ fi
 read -er -p "To server account@host:  server
 read -er -p "To target directory e.g. subdomain or public_html: " dir
 
-if [ ! -z "$db1" ] then
+if [ ! -z "$db1" ] 
+then
   mysqldump --add-drop-table --user=$account --password="${pass}" $db1 > /tmp/${account}_db.sql
   cat /tmp/${account}_db.sql|ssh $server "mysql --user=$user2 --password="${pass}" $database2  --"
   rm -rf /tmp/${account}_db.sql
@@ -22,7 +24,8 @@ cat /tmp/${account}.tar.gz | ssh $server "cd ${dir};tar -zxf -"
 
 rm -rf /tmp/$account.tar.gz
 
-if [[ "$wp" =~ ^([yY][eE][sS]|[yY])+$ ]] then
+if [[ "$wp" =~ ^([yY][eE][sS]|[yY])+$ ]] 
+then
   ssh $server "cd ${dir}; wp config set DB_NAME \"${database2}i\""
   ssh $server "cd ${dir}; wp config set DB_USER \"${user2}\""
 fi
