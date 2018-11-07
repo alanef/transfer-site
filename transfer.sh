c5274fa699cc4dcefa973c1db24ecf426cb830fa
Alan Fuller <alan@fullworks.net>
2018-11-07 18:03:09 +0000

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,29 +1,29 @@
 #!/bin/bash  
-printf "From Account /home/account, without /home/: "
-read -e account
-printf "From Database: "
-read -e db1
-printf "DB Pasword ( same on both): "
-read -e  pass
-printf "To db name: "
-read -e database2
-printf "To db user: "
-read -e user2
-printf "To server account:host: "
-read -e server
-printf "To target directory e.g. subdomain or public_html: "
-read -e dir
+read -er -p  "From Account /home/account, without /home/: " account
+read -er -p "From Database ( blank for none ): " db1
+if [ ! -z "$db1" ] then
+  read -er -p "DB Pasword ( same on both): " pass
+  read -er -p "To db name: " database2
+  read -er -p "To db user: " user2
+  read -er -p "Wordpress and wp cli on remote(y/n): " wp
+fi
+read -er -p "To server account@host:  server
+read -er -p "To target directory e.g. subdomain or public_html: " dir
 
-mysqldump --add-drop-table --user=$account --password="${pass}" $db1 > /tmp/${account}_db.sql
-cat /tmp/${account}_db.sql|ssh $server "mysql --user=$user2 --password="${pass}" $database2  --"
+if [ ! -z "$db1" ] then
+  mysqldump --add-drop-table --user=$account --password="${pass}" $db1 > /tmp/${account}_db.sql
+  cat /tmp/${account}_db.sql|ssh $server "mysql --user=$user2 --password="${pass}" $database2  --"
+  rm -rf /tmp/${account}_db.sql
+fi
 
 tar -czf /tmp/${account}.tar.gz -C /home/$account/public_html .
 cat /tmp/${account}.tar.gz | ssh $server "cd ${dir};tar -zxf -"
 
 
 rm -rf /tmp/$account.tar.gz
-rm -rf /tmp/${account}_db.sql
 
-ssh $server "cd ${dir}; wp config set DB_NAME \"${database2}i\""
-ssh $server "cd ${dir}; wp config set DB_USER \"${user2}\""
+if [[ "$wp" =~ ^([yY][eE][sS]|[yY])+$ ]] then
+  ssh $server "cd ${dir}; wp config set DB_NAME \"${database2}i\""
+  ssh $server "cd ${dir}; wp config set DB_USER \"${user2}\""
+fi
 
